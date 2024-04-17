568c40fdfaf116320aea21853b50212161719f86
Songhee Heo <songheeheo@Songheeui-MacBookPro.local>
2024-04-17 11:12:36 +0900

    header app.tsx추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import styled from '@emotion/styled';
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import { DataView } from 'pages/DataView';
 import { ToDoInput } from 'pages/ToDoInput';
 import { ToDoListContextProvider } from 'context/ToDoList';
@@ -19,10 +19,30 @@ const NotFound = styled.div`
   text-align: center;
 `;
 
+const Header = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+  background-color: #304ffe;
+  padding: 8px 0;
+  margin: 0;
+`;
+
+const StyledLink = styled(Link)`
+  color: #ffffff;
+  font-size: 20px;
+  text-decoration: none;
+`;
+
 function App() {
   return (
     <Container>
       <ToDoListContextProvider>
+        <Header>
+          <StyledLink to="/">할 일 목록 앱</StyledLink>
+        </Header>
         <Routes>
           <Route path="/" element={<DataView/>} />
           <Route path="/add" element={<ToDoInput />} />
