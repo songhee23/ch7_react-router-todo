268c9ed4cea403cd9d10711bd8ccc57bc4e0a6a4
Songhee Heo <songheeheo@Songheeui-MacBookPro.local>
2024-04-17 15:28:39 +0900

    Header component분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import styled from '@emotion/styled';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { DataView } from 'pages/DataView';
 import { ToDoInput } from 'pages/ToDoInput';
 import { ToDoListContextProvider } from 'context/ToDoList';
+import { Header } from 'components/Header';
 
 const Container = styled.div`
   height: 100vh;
@@ -19,30 +20,11 @@ const NotFound = styled.div`
   text-align: center;
 `;
 
-const Header = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  text-align: center;
-  background-color: #304ffe;
-  padding: 8px 0;
-  margin: 0;
-`;
-
-const StyledLink = styled(Link)`
-  color: #ffffff;
-  font-size: 20px;
-  text-decoration: none;
-`;
-
 function App() {
   return (
     <Container>
       <ToDoListContextProvider>
-        <Header>
-          <StyledLink to="/">할 일 목록 앱</StyledLink>
-        </Header>
+        <Header />
         <Routes>
           <Route path="/" element={<DataView/>} />
           <Route path="/add" element={<ToDoInput />} />
